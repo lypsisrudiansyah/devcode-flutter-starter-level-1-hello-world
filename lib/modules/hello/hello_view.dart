07d432d10b934f7c346e2ac0eba691b00ddeecd8
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-26 20:36:45 +0700

    Initial commit

---
+++ lib/modules/hello/hello_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HelloView extends StatelessWidget {
+  const HelloView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/png/devcode-logo.png', key: const Key('devcode-logo'), width: 200, height: 200),
+          const SizedBox(height: 15,),
+          const Align(
+            alignment: Alignment.center, key: Key('devcode-title'), child: Text("I'm ready for the next challenge!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+          )
+        ],
+      ),
+    );
+  }
+}
